35073574fceb13a9e99e974b663f027006455ee1
itskleb <itskleb@gmail.com>
2023-04-14 11:49:49 -0400

    Add files via upload

--- map_builder.py
+++ map_builder.py
@@ -188,7 +188,7 @@ locals = fl.FeatureGroup(name='Neighborhoods',show=False)
 style = {'fillColor':'#000000','color':'#000000', 'weight':1}
 
 for _ , item in hoods.iterrows():
-    if item['neighborhood'] in units.neighborhood.unique().tolist():
+    if item['neighborhood'] in units.neighborhood.unique().to_list():
         neigh = fl.GeoJson(data=item['geometry'], name='Neighborhoods',style_function= lambda x:style)
         fl.Popup(item['neighborhood']).add_to(neigh)
         neigh.add_to(locals)
